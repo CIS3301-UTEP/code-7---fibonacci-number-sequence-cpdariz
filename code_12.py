00a6582a8a86ffde0262a95d7b5fae02540f8416
cpdariz <cparmendariz@miners.utep.edu>
2024-11-03 21:24:41 -0700

    One and done hopefully

--- code_12.py
+++ code_12.py
@@ -1,8 +1,20 @@
 def get_fibonacci_number(position):
-    pass #Remove this line and insert your code here. Do not forget this function implements recursion.
-
+    if position == 0:
+        return 0
+    if position == 1:
+        return 1
+    else:
+        return get_fibonacci_number(position - 1) + get_fibonacci_number(position - 2)
+    
 def get_fibonacci_number_sequence(number):
-    pass #Remove this line and insert your code here. Do not forget to use get_fibonacci_number to create your list of numbers.
-
+    sequence = []
+    for i in range(1,number+1):
+        sequence.append(get_fibonacci_number(i))
+    return sequence
+    
 if __name__ == "__main__":
-    pass #Remove this line and insert your code to test your Fibonacci function here
+    position = 5
+    print(f"Fibonacci number at position {position} is: {get_fibonacci_number(position)}")
+
+    number_of_elements = 7
+    print(f"Fibonacci sequence for {number_of_elements} elements: {get_fibonacci_number_sequence(number_of_elements)}")
